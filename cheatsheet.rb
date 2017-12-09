5306ec872c1409fce22e000e460fb61efcb9b91b
jimmymixer <jaepark75@gmail.com>
2017-12-09 13:49:09 -0500

    RecipeApp, Gems bootstrap and jquery

--- cheatsheet.rb
+++ cheatsheet.rb
@@ -3,3 +3,9 @@ cd RecipeApp
 rails s
 rails g scaffold Recipe title:string description:text
 rails db:migrate
+gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
+gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
+bundle
+scss
+@import "bootstrap-sprockets";
+@import "bootstrap";
